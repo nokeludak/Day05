b7a54cd53dfd8069a1833097676f61d039272d8f
nokeludak <ksmajic996@gmail.com>
2021-03-13 01:53:39 +0100

    Dodao ex07

---
+++ ex07/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    // Only change code below this line
+    var myAdjective = "the best!";
+    var myString = "Mistral is ";
+    myString += myAdjective;
+    // Only change code above this line
+    return myString;
+}
+console.log(main());
+module.exports = main;
